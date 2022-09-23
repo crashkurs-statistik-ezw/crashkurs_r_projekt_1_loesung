df3d15495a0c43862f4e5141e970a4ab33c84e83
Christian <burkhart.christian@gmail.com>
2022-09-23 21:13:02 +0200

    review Chrstian finished

--- analysis/datenbereinigung.R
+++ analysis/datenbereinigung.R
@@ -1,22 +1,38 @@
 # 1.1 Setup -------------------------------------------------------------
 # 1.1.1 Pakete laden
-# (Installiere und) lade die Pakete janitor und haven
+# (Installiere und) lade die Pakete tidyverse, janitor und haven
+# Tipp: Pakete werden mit dem Befehl install.packages installiert:
+#  z. B. install.packages("janitor")
+library(tidyverse)
 library(janitor)
 library(haven)
+
+
+# 1.1.2 Geladene Pakete ansehen
+# Führe den folgenden Befehl aus und prüfe, ob die Pakete
+# tidyverse, janitor und haven unter "other attached packages"
+# zu finden sind. Alle Funktionen aus diesen Paketen können
+# in deinem Skript verwendet werden.
+sessionInfo()
+
 
 # 2.2 Daten einlesen ------------------------------------------------------
 
 # 2.2.1 Daten einlesen
 # * Lese den Datensatz data/hr_data.csv ein
-# * Speichere den Datensatz in der Variable hr_data
+# * Speichere den Datensatz in der Variable "hr_data"
+# * Tipp: read_csv wird verwendet, wenn die Werte in der CSV-Datei
+#   durch ein Komma getrennt sind. read_csv2 benutzt du fuer
+#   CSV-Dateien, die Werte mit einem Semikolon trennen (siehe ?read_csv)
 hr_data <- read_csv("data/hr_data.csv")
 
 
 # 2.3 Daten bereinigen ---------------------------------------------
 
 # 2.3.1 Variablennamen bereinigen
-# * Wandle die Variablennamen mit clean_names in snake case um
-# * Speichere den bereinigten Datensatz in der Variable hr_cleaned
+# * Reinige die Variablennamen mit der Funktion clean_names 
+#   aus dem Paket jantior (siehe ?clean_names)
+# * Speichere den bereinigten Datensatz in der Variable "hr_cleaned"
 hr_cleaned <- hr_data %>% 
   clean_names(case = "snake")
 
@@ -24,13 +40,13 @@ hr_cleaned <- hr_data %>%
 # 2.4 Datenexport ---------------------------------------------------------
 
 # 2.4.1 CSV-Datei exportieren
-# * Exportiere den Datensatz in den Ordner data/cleaned
-# * Speichere die Daten unter data/cleaned/hr_cleaned.csv
+# * Exportiere den Datensatz hr_cleaned in den Ordner data/cleaned
+# * Speichere den Datensatz  unter data/cleaned/hr_cleaned.csv
 write_csv(hr_cleaned, "data/cleaned/hr_cleaned.csv")
 
 
 # 2.4.2 SAV-Datei exportieren
 # * Um die Daten in SPSS zu nutzen, exportiere den gereinigten Datensatz mit der
-#   Funktion write_sav
+#   Funktion write_sav aus dem Paket haven (siehe ?write_sav)
 # * Speichere die Daten unter data/cleaned/hr_cleaned.sav
 write_sav(hr_cleaned, "data/cleaned/hr_cleaned.sav")
--- analysis/datenexploration.R
+++ analysis/datenexploration.R
@@ -1,41 +1,59 @@
-# 2.5 Daten explorieren  ------------------------------------------------
 
-# 2.5.1 Daten anzeigen
-# Lasse dir den Datensatz einmal mit der Funktion glimpse und einmal mit der
-# Funktion view anzeigen
-glimpse(hr_data)
-view(hr_data)
+# 2.5 Setup ---------------------------------------------------------------
 
-# 2.5.2 Reihen und Spalten zählen
-# Wie viele Reihen und Spalten hat der Datensatz? Nutze die Funktionen nrow und
-# ncol
+# 2.5.1 Lade den Datensatz
+# * Lade den Datensatz data/cleaned/hr_cleaned.csv mit der Funktion read_csv
+# * Speichere den Datensatz in der Variable hr_cleaned
+hr_cleaned <- read_csv("data/cleaned/hr_cleaned.csv")
+
+
+# 2.6 Daten explorieren  ------------------------------------------------
+
+# 2.6.1 Daten anzeigen
+# Zeige dir den Datensatz mit der Funktion glimpse und view an
+glimpse(hr_cleaned)
+view(hr_cleaned)
+
+
+# 2.6.2 Reihen und Spalten zählen
+# Wie viele Reihen und Spalten hat der Datensatz? Nutze die Funktionen nrow, 
+# ncol und dim
 nrow(hr_cleaned)
 ncol(hr_cleaned)
+dim(hr_cleaned)
 
-# 2.5.3 Geschlechterverteilung
+
+# 2.6.3 Geschlechterverteilung
 # Wie viele Männer und Frauen sind im Datensatz?
 hr_cleaned %>%
   count(gender)
 
-# 2.5.4 Durchschnittliches Alter berechnen
-# Bestimme Mittelwert, Median und Modalwert des Alters der Mitarbeitenden
+
+# 2.6.4 Durchschnittliches Alter berechnen
+# Bestimme den Mittelwert, Median und Modalwert des Alters der Mitarbeitenden
 mean(hr_cleaned$age, na.rm = TRUE)
-median(hr_cleaned$age)
-table(hr_cleaned$age)
+median(hr_cleaned$age, na.rm = TRUE)
+hr_cleaned %>% 
+  count(age, sort = TRUE)
 
-# 2.5.5 Table lesen
+
+# 2.6.5 Table lesen
 # Wie viele Mitarbeitende sind 29 Jahre alt und wie viele sind 60 Jahre alt?
+# 29 -> 68
+# 60 -> 5
 
-# 2.5.6 Altersverteilung bestimmen
-# Bestimme Spannweite, Varianz und Standardabweichung des Alters
+
+# 2.6.6 Altersverteilung bestimmen
+# Bestimme die Spannweite, Varianz und Standardabweichung des Alters
 range(hr_cleaned$age)
 sd(hr_cleaned$age)
 var(hr_cleaned$age)
 
-# 2.5.7 Work-Life-Balance bestimmen
+
+# 2.6.7 Work-Life-Balance bestimmen
 # * Untersuche, ob sich die Work-Life-Balance mit dem Alter verändert 
 # * Berechne den Mittelwert für alle Ausprägungen der Variable work_life_balance
-#   mit den Funktionen group_by summarise
+#   mit den Funktionen group_by und summarise
 # * Sind Mitarbeitende mit einer schlechten Work-Life-Balance durchschnittlich
 #   älter als Mitarbeitende mit einer schlechten Work-Life-Balance?
 hr_cleaned %>%
@@ -45,50 +63,50 @@ hr_cleaned %>%
   )
 
 
-# 2.6 Daten visualisieren -----------------------------------------------------
+# 2.7 Daten visualisieren -----------------------------------------------------
 
-# 2.6.1 Altersverteilung visualisieren
-# * Erstelle ein Histogramm sowie einen Boxplot zur Altersverteilung der
-#   Mitarbeitenden
-# * Nutze die Funktion ggplot und bestimme mit Hilfe von aesthetics, dass die
-#   Variable age auf der x-Achse ist
-# * Nutze die geoms 'geom_histogram' und 'geom_boxplot'
-# * Welche Vorteile haben die beiden Visualisierungen jeweils? Kann man aus
-#   einer der beiden mehr lesen als aus der anderen?
+# 2.7.1 Altersverteilung visualisieren
+# * Erstelle ein Histogramm zur Altersverteilung der Mitarbeitenden
+# * Nutze die Funktion ggplot und lege die Variable age auf die x-Achse
+# * Nutze das geom 'geom_histogram' 
+# * Wie könnte man die Darstellung des Histograms weiter verbessern?
 ggplot(hr_cleaned, aes(x = age)) +
-  geom_histogram()
+  geom_histogram(fill = "grey80", color = "grey20",
+                 binwidth = 1) +
+  scale_y_continuous(expand = expansion(c(0, 0.05))) +
+  labs(
+    x = "Alter",
+    y = "Häufigkeit"
+  )
 
-ggplot(hr_cleaned, aes(x = age)) +
-  geom_boxplot()
 
-# 2.6.2 Visualisierungen speichern
-# * Speichere die Visualisierungen im R-Projekt ab unter dem Pfad
-#   images/histogram_verteilung_alter.png und images/boxplot_verteilung_alter
+# 2.7.2 Visualisierung speichern
+# * Speichere die Visualisierung im R-Projekt ab unter dem Pfad
+#   images/histogram_verteilung_alter.png
 # * ggsave speichert immer die letzte Visualisierung ab, du musst also den Code
 #   jeweils durchführen, bevor du die Visualisierung speicherst
 ggsave("images/histogram_verteilung_alter.png",
        width = 8, height = 5, dpi = 300)
-ggsave("images/boxplot_verteilung_alter.png",
-       width = 8, height = 5, dpi = 300)
 
-# 2.6.3 Punktdiagramm zum Einkommen erstellen
+
+# 2.7.3 Streudiagramm zum Einkommen erstellen
 # Untersuche, ob ältere Mitarbeiter mehr verdienen als jüngere Mitarbeiter
-# * Erstelle ein Punktdiagramm, das die Variable age auf der x-Achse und die
+# * Erstelle ein Streudiagramm, das die Variable age auf der x-Achse und die
 #   Variable monthly_income auf der y-Achse darstellt
-# * Gib den einzelnen Punkten mit Hilfe der aesthetics-Funktion eine Farbe
-#   abhängig der nominalskalierten Variable department, um zu zeigen, wie die
-#   Einkommensverteilung in den einzelnen Abteilungen aussieht
+# * Erstelle mit facet_wrap ein eigenes Streudiagramm für jede Abteilung
 # * Füge mit Hilfe von labs einen sinnvollen Titel und Achsentitel hinzu
 ggplot(hr_cleaned, aes(x = age, y = monthly_income)) +
-  geom_point(aes(color = department)) +
+  geom_point(alpha = .7) +
+  facet_wrap(vars(department)) +
   labs(
-    title = "Punktdiagramm des Monatseinkommens und Alters",
+    title = "Beziehung zwischen Monatseinkommen und Alter\nin den Abteilungen",
     x     = "Alter",
     y     = "Monatseinkommen"
     )
 
-# 2.6.4
+
+# 2.7.4
 # Speichere die Visualisierung im R-Projekt ab unter dem Pfad
-# images/punktdiagramm_monatseinkommen_alter.png
-ggsave("images/punktdiagramm_monatseinkommen_alter.png", width = 8,
+# images/streudiagramm_monatseinkommen_alter.png
+ggsave("images/streudiagramm_monatseinkommen_alter.png", width = 8,
        height = 5, dpi = 300)
